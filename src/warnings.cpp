8f9b8190b4227286a9dff0d9b09baa2a51bbd97d
hideoussquid <a987@mac.com>
2017-04-03 01:37:28 +0800

    fixed genesis values

--- src/warnings.cpp
+++ src/warnings.cpp
@@ -53,8 +53,8 @@ std::string GetWarnings(const std::string& strFor)
     LOCK(cs_warnings);
 
     if (!CLIENT_VERSION_IS_RELEASE) {
-        strStatusBar = "This is a pre-release test build - use at your own risk - do not use for mining or merchant applications";
-        strGUI = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications");
+        strStatusBar = "";
+        strGUI = _("");
     }
 
     if (GetBoolArg("-testsafemode", DEFAULT_TESTSAFEMODE))
